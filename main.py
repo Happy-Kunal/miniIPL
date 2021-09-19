c2ea5f315f3aabd05801619383191d72e395ec1f
Kunal Sharma <happykunal@protonmail.com>
2021-09-19 23:29:41 +0530

    Completed the Bot

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import sys
+import json
 import time
 import datetime
 
@@ -5,7 +7,41 @@ import poll
 import matchInfo
 
 def main():
-    schedule = matchInfo.get_next_match()
-    for match in schedule:
+    now = datetime.datetime.now()
+    current_time = now.strftime("%H:%M")
+    if current_time == "12:00":
+        schedule = matchInfo.get_next_match()
+        for matchNo, matchData in schedule.items():
+            options = matchData["teams"]
+            poll.send_mesg(json.dumps(matchData, indent=4))
+            poll.send_poll(question= f"who will win {matchNO}", options=options, reply_to_message_id=poll.get_last_update_message_id())
+
+        time.sleep(61) # sleeping for 61 secs so that condition doesn't become true twice on same day
+
+    live_score = matchInfo.ipl_live_score()
+    if len(live_score) > 0:
+        poll.send_mesg(json.dumps(live_score, indent=4))
+        
+        matchNo = matchInfo.get_next_match().items()[0][0]
+        polls = poll.get_sent_polls()
+        for poll in polls:
+            if matchNo in poll["poll"]["question"]:
+                poll.close_poll(chat_id=poll["chat_id"], message_id= poll["message_id"])
+                
+    else:
+        print("Noting To Do :-(")
+
+
+if __name__ == "__main__":
+    while True:
+        try:
+            main()
+            time.sleep(10)
+        except KeyboardInterrupt:
+            sys.exit(1)
+        except Exception as e:
+            print(e)
+
+
 
 
--- poll.py
+++ poll.py
@@ -11,10 +11,12 @@ except:
     print("Error parsing config file")
     sys.exit(2)
 
-# r = requests.post(f"https://api.telegram.org/bot{TOKEN}/getUpdates").json()
-# print(r)
+def get_last_update_message_id():
+    r = requests.post(f"https://api.telegram.org/bot{TOKEN}/getUpdates")
+    if r.status_code == 200 and r.json()["ok"]:
+        return r.json()["result"][-1]["message"]["message_id"]
 
-def send_poll(question : str, options) -> None:
+def send_poll(question : str, options, reply_to_message_id:int) -> None:
     """
         This function reads the chat id from the config.json file
         and send the question and options to every chat_id
@@ -25,7 +27,8 @@ def send_poll(question : str, options) -> None:
         "options" : json.dumps(options),
         "is_anonymous" : False,
         "type" : "quiz",
-        "correct_option_id" : 0
+        "correct_option_id" : 0,
+        "reply_to_message_id": reply_to_message_id
     }
 
     for i in CHAT_ID:
@@ -41,4 +44,71 @@ def send_poll(question : str, options) -> None:
                     print("error while sending the poll")
         except:
             print("Error in sending file to chat_id: ", i)
+
+
+def send_mesg(text: str) -> None:
+    data = {"text": text}
+    for ID in CHAT_ID:
+        data["chat_id"] = ID
+        try:
+            for _ in range(20):
+                r = requests.post(f"https://api.telegram.org/bot{TOKEN}/sendMessage", data = data)
+                req_result = r.json()
+                if r.status_code == 200 and req_result["ok"]:
+                    print("Poll send successfuly")
+                    break
+                else:
+                    print("error while sending the poll")
+        except:
+            print("Error in sending file to chat_id: ", i)
+
+
+
+def get_sent_polls():
+    runs = 0
+    while True:
+        try:
+            r = requests.post(f"https://api.telegram.org/bot{TOKEN}/getUpdates")
+            if r.status_code != 200:
+                continue
+            elif not r.json()["ok"]:
+                continue
+            else:
+                results = r.json()["result"]
+                output_result = []
+                poll_ids = []
+                for index, update in enumerate(results):
+                    if poll in update and update["poll"]["id"] not in poll_ids:
+                        poll_ids.append(update["poll"]["id"])
+                        previous_message = results[index - 1]["message"]
+                        update["chat_id"] = previous_message["chat"]["id"]
+                        update["message_id"] = previous_message["message_id"]
+                        output_result.append(update)
+                return output_result
+
+        except Exception as e:
+            print("Error occurred:\n", e)
+            if run > 20:
+                return []
+            else:
+                run += 1
+def close_poll(chat_id, message_id):
+    poll_data = json.dumps({
+        "chat_id" : chat_id,
+        "message_id" : message_id,
+    })
+
+    for _ in range(20):
+        try:
+            r = requests.post(f"https://api.telegram.org/bot{TOKEN}/stopPoll", data = poll_data)
+            if r.status_code != 200:
+                continue
+            elif not r.json()["ok"]:
+                continue
+            else:
+                print("Ended poll", poll_data)
+                return r.json()
+
+        except Exception as e:
+            print("Error occurred:\n", e)
 
